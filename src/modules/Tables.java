b37d24f9d4039a36202cd50c282dccab223c3d24
Benjamin B <s305338@stud.hioa.no>
2016-08-19 14:53:19 +0200

    Added code belonging to tasks for 1.1.2 and 1.1.3

--- src/modules/Tables.java
+++ src/modules/Tables.java
@@ -22,17 +22,17 @@ public final class Tables {
     public static int max_optimized(int[] table) {
         if (table.length < 1) throw new NoSuchElementException("Empty table");
 
-        int m = 0;
-        int maxval = table[0];
+        int max_pos = 0;
+        int max_val = table[0];
 
-        for (int i = 1; i < table.length; i++) {
-            if (table[i] > maxval) {
-                m = i;
-                maxval = table[m];
+        for (int index = 1; index < table.length; index++) {
+            if (table[index] > max_val) {
+                max_pos = index;
+                max_val = table[max_pos];
             }
         }
 
-        return m;
+        return max_pos;
     }
 
     public static int min(int[] table) {
@@ -42,6 +42,22 @@ public final class Tables {
 
         for (int current_pos = 1; current_pos < table.length; current_pos++) {
             if (table[current_pos] < table[min_pos]) min_pos = current_pos;
+        }
+
+        return min_pos;
+    }
+
+    public static int min_optimized(int[] table) {
+        if (table.length < 1) throw new NoSuchElementException("Empty table");
+
+        int min_pos = 0;
+        int min_val = table[0];
+
+        for (int index = 1; index < table.length; index++) {
+            if (table[index] < min_val) {
+                min_pos = index;
+                min_val = table[min_pos];
+            }
         }
 
         return min_pos;
@@ -58,16 +74,29 @@ public final class Tables {
         for (int current_pos = 1; current_pos < table.length; current_pos++) {
             int current_value = table[current_pos];
 
-            if (current_value < max_value) {
+            if (current_value > max_value) {
                 max_pos = current_pos;
                 max_value = table[max_pos];
-            } else if (current_value > min_value) {
+            } else if (current_value < min_value) {
                 min_pos = current_pos;
                 min_value = table[min_pos];
             }
         }
 
         int[] result = {min_pos, max_pos};
+        return result;
+    }
+
+    public static final int[] minmax_lazy(int[] table) {
+        int[] result = {min_optimized(table), max_optimized(table)};
+        return result;
+    }
+
+    public static long factorial(int number) {
+        int result = number;
+        for (int index = 1; index < number-1; index++) {
+            result = result * (number - index);
+        }
         return result;
     }
 }
