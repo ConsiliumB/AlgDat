bd60a574e8c8e8dc7b6598a0c1fc3cd7ed3442b2
Benjamin B <s305338@stud.hioa.no>
2016-08-20 21:43:31 +0200

    Renamed default program to Main.

--- src/modules/Tables.java
+++ src/modules/Tables.java
@@ -99,5 +99,57 @@ public final class Tables {
         }
         return result;
     }
+
+    public static int last_max_sentinel(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+
+        for (int i = 0; ; i++)         // i starter med 0
+            if (a[i] >= maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
+                {
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return temp >= maksverdi ? sist : m;   // er siste størst?
+                }
+                else
+                {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
+                }
+            }
+    } // maks
+
+    public static int first_max_sentinel(int[] a)  // versjon 3 av maks-metoden
+    {
+        int sist = a.length - 1;       // siste posisjon i tabellen
+        int m = 0;                     // indeks til største verdi
+        int maksverdi = a[0];          // største verdi
+        int temp = a[sist];            // tar vare på siste verdi
+        a[sist] = 0x7fffffff;          // legger tallet 2147483647 sist
+
+        
+
+        for (int i = 0; ; i++)         // i starter med 0
+            if (a[i] > maksverdi)       // denne blir sann til slutt
+            {
+                if (i == sist)             // sjekker om vi er ferdige
+                {
+                    a[sist] = temp;          // legger siste verdi tilbake
+                    return temp > maksverdi ? sist : m;   // er siste størst?
+                }
+                else
+                {
+                    maksverdi = a[i];        // maksverdi oppdateres
+                    m = i;                   // m oppdateres
+                }
+            }
+    } // maks
+
+
 }
 
---
+++ src/programs/Main.java
@@ -0,0 +1,71 @@
+package programs;
+
+import modules.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("... Initiating AlgDat ...\n");
+
+
+        tables_factorial();
+        tables_minmax();
+        tables_minmax_lazy();
+        tables_max();
+        tables_first_max_sentinel();
+        tables_last_max_sentinel();
+    }
+
+    public static void tables_factorial() {
+        long tid = System.nanoTime();
+        long result = Tables.factorial(5);
+        tid = System.nanoTime() - tid;
+        System.out.println("Factorial calculation took "+tid+"ns.");
+        System.out.println("Found that 5! = "+result+".\n");
+    }
+
+    public static void tables_minmax() {
+        int[] table = {4, 6, 8, 2, 3, 1, 5, 9};
+        long tid = System.nanoTime();
+        int[] result = Tables.minmax(table);
+        tid = System.nanoTime() - tid;
+        System.out.println("Optimized Min & Max position calculation took "+tid+"ns.");
+        System.out.println("Given " + Arrays.toString(table) + " found " + Arrays.toString(result) + ".\n");
+    }
+
+    public static void tables_minmax_lazy() {
+        int[] table = {4, 6, 8, 2, 3, 1, 5, 9};
+        long tid = System.nanoTime();
+        int[] result = Tables.minmax_lazy(table);
+        tid = System.nanoTime() - tid;
+        System.out.println("Lazy Min & Max position calculation took "+tid+"ns.");
+        System.out.println("Given " + Arrays.toString(table) + " found " + Arrays.toString(result) + ".\n");
+    }
+
+    public static void tables_max() {
+        int[] table = {4, 6, 9, 2, 3, 1, 5, 9};
+        long tid = System.nanoTime();
+        int result = Tables.max(table);
+        tid = System.nanoTime() - tid;
+        System.out.println("Max calculation took "+tid+"ns.");
+        System.out.println("Given " + Arrays.toString(table) + " found " + result + ".\n");
+    }
+
+    public static void tables_first_max_sentinel() {
+        int[] table = {4, 6, 9, 2, 3, 1, 5, 9};
+        long tid = System.nanoTime();
+        int result = Tables.first_max_sentinel(table);
+        tid = System.nanoTime() - tid;
+        System.out.println("Max calculation returning first value using Sentinel took "+tid+"ns.");
+        System.out.println("Given " + Arrays.toString(table) + " found " + result + ".\n");
+    }
+
+    public static void tables_last_max_sentinel() {
+        int[] table = {4, 6, 9, 2, 3, 1, 5, 9};
+        long tid = System.nanoTime();
+        int result = Tables.last_max_sentinel(table);
+        tid = System.nanoTime() - tid;
+        System.out.println("Max calculation returning last value using Sentinel took "+tid+"ns.");
+        System.out.println("Given " + Arrays.toString(table) + " found " + result + ".\n");
+    }
+}
